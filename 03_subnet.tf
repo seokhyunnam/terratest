a28c8e1ad93199f34e3f2ea01b19e346278ae0c7
shnam <hyun322@gmail.com>
2021-11-10 19:26:03 +0900

    'step5'

---
+++ 03_subnet.tf
@@ -0,0 +1,39 @@
+# 가용영역 a의 public subnet
+resource "aws_subnet" "shnam_puba" {
+  vpc_id = aws_vpc.shnam_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "shnam-puba"
+  }
+}
+
+# 가용영역 a의 private subnet
+resource "aws_subnet" "shnam_pria" {
+  vpc_id = aws_vpc.shnam_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "shnam-pria"
+  }
+}
+
+# 가용영역 c의 public subnet
+resource "aws_subnet" "shnam_pubc" {
+  vpc_id = aws_vpc.shnam_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "shnam-pubc"
+  }
+}
+
+# 가용영역 c의 private subnet
+resource "aws_subnet" "shnam_pric" {
+  vpc_id = aws_vpc.shnam_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "shnam-pric"
+  }
+}
