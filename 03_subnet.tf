fc1e2d5665b6a0418179f0fa1ef0af6ed8836afd
shnam <hyun322@gmail.com>
2021-11-12 16:16:26 +0900

    'step2'

--- 03_subnet.tf
+++ 03_subnet.tf
@@ -37,3 +37,23 @@ resource "aws_subnet" "shnam_pric" {
     Name = "shnam-pric"
   }
 }
+
+# 가용영역 a의 private DB subnet
+resource "aws_subnet" "shnam_pridba" {
+  vpc_id = aws_vpc.shnam_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "shnam-pridba"
+  }
+}
+
+# 가용영역 c의 private DB subnet
+resource "aws_subnet" "shnam_pridbc" {
+  vpc_id = aws_vpc.shnam_vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "shnam-pridbc"
+  }
+}
