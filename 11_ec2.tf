5461f4af68bb363509f0a73e63263b94d9ea7b35
shnam <hyun322@gmail.com>
2021-11-12 17:52:33 +0900

    'step2'

--- 11_ec2.tf
+++ 11_ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "shnam_weba" {
   instance_type          = "t2.micro"
   key_name               = "shnam2-key"
   availability_zone      = "ap-northeast-2a"
-  private_ip             = "10.0.0.11"
+  # private_ip             = "10.0.0.11"
   subnet_id              = aws_subnet.shnam_puba.id          #public_subnet 1의 id
   vpc_security_group_ids = [aws_security_group.shnam_sg.id]
   user_data              = file("./install.sh")              #워드프레스 관련 문서파일을 해당 위치로 위치시키고
@@ -32,7 +32,7 @@ resource "aws_instance" "shnam_weba" {
 resource "aws_eip" "shnam_web_eip" {
   vpc = true
   instance                    = aws_instance.shnam_weba.id
-  associate_with_private_ip   = "10.0.0.11"
+ # associate_with_private_ip   = "10.0.0.11"
   depends_on                  = [
     aws_internet_gateway.shnam_ig
     ]
